a7caff0f686e720ab31a37cf9a163afe75d16190
Iwan Pavord <ipavord@gmail.com>
2023-08-29 12:06:41 +0100

    completed basic table

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,21 @@
+require "open-uri"
+require "json"
+
 class PlayersController < ApplicationController
   def index
+    @players = Player.all
+    # Getting API data for each player and putting into array of hashes
+    @live_data = []
+    general_url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    general_info = JSON.parse(URI.open(general_url).read)
+    @players.each_with_index do |player, index|
+      player_info = general_info["elements"][index]
+      @live_data << {
+        form: player_info["form"],
+        price: player_info["now_cost"].to_f / 10,
+        selected: player_info["selected_by_percent"].to_f,
+        ict: player_info["ict_index"].to_f
+      }
+    end
   end
 end
