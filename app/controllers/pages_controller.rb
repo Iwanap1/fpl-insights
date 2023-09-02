f7e79536eae1037f8ba4ecd78ff1808a12c50de2
Iwan Pavord <ipavord@gmail.com>
2023-09-02 10:42:42 +0100

    added rankings graph but cant invert y axis

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,17 +14,51 @@ class PagesController < ApplicationController
     @league_manager_ids = league_api["standings"]["results"].map{ |user| user["entry"] }
     @league_manager_names = league_api["standings"]["results"].map{ |user| user["player_name"] }
     @graph_data = []
-    @league_manager_ids.each do |id|
+    all_user_data = []
+    @league_manager_ids.each_with_index do |id, index|
       @graph_data << get_points_data(id)
+      user_data = get_user_data(id)
+      user_data[:name] = @league_manager_names[index]
+      all_user_data << user_data
     end
+    @ranks = get_ranks(all_user_data)
   end
 
   def get_points_data(id)
     user_points_data = []
     history_api = JSON.parse(URI.open("https://fantasy.premierleague.com/api/entry/#{id}/history/").read)
-    (1..@current_gw).to_a.each do |gw|
-      user_points_data << [history_api["current"][gw - 1]["event"], history_api["current"][gw - 1]["total_points"]]
+    history_api["current"].each do |gw|
+      user_points_data << [gw["event"], gw["total_points"]]
     end
     return user_points_data
+  end
+
+  def get_user_data(id)
+    history_api = JSON.parse(URI.open("https://fantasy.premierleague.com/api/entry/#{id}/history/").read)
+    user_data = {}
+    (1..@current_gw).to_a.each do |gw|
+      user_data["#{gw}"] = 0
+    end
+    history_api["current"].each do |gw|
+      user_data["#{gw["event"]}"] = gw["total_points"]
+    end
+    return user_data
+  end
+
+  def get_ranks(all_user_data)
+    (1..@current_gw).to_a.each do |gw|
+      all_user_data.sort_by { |user| user["#{gw}"] }.reverse.each_with_index do |user, index|
+        user["#{gw}"] = index + 1
+      end
+    end
+    all_rank_data = []
+    all_user_data.each do |user|
+      user_array = []
+      (1..@current_gw).each do |gw|
+        user_array << [gw, user["#{gw}"]]
+      end
+      all_rank_data << [user[:name], user_array]
+    end
+    return all_rank_data
   end
 end
