2b68edefa6c3d121e52dac2a8dd00c1e9d5c5671
Iwan Pavord <ipavord@gmail.com>
2023-09-05 14:36:46 +0100

    fixturees

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -9,5 +9,13 @@ class Fixture < ApplicationRecord
   def self.update_all
     fixture_url = "https://fantasy.premierleague.com/api/fixtures/"
     fixture_info = JSON.parse(URI.open(fixture_url).read)
+    Fixture.all.each do |fixture|
+      api_fixture = fixture_info.find { |f| f["team_a"] == fixture.away_team_id && f["team_h"] == fixture.home_team_id }
+      fixture.date = Date.parse(fixture["kickoff_time"].split("T")[0])
+      # Continue here, fixture == db, api_fixture == api
+
+
+      fixture.save
+    end
   end
 end
